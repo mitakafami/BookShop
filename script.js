4db3d52d5db0ed7814bf31edf0b907e0976dc1e8
n1er4 <102894287+n1er4@users.noreply.github.com>
2022-05-15 20:34:17 +0400

    feat: add ready

--- script.js
+++ script.js
@@ -5,3 +5,31 @@ fetch('../assets/JSON.json')
         .then(data => {
             console.log(data);
         });
+
+if (document.readyState == 'loading') {
+    document.addEventListener('DOMContentLoaded', ready)
+} else {
+    ready()
+}
+
+function ready() {
+    var removeCartItemButtons = document.getElementsByClassName('remove-button')
+    for (var i = 0; i < removeCartItemButtons.length; i++) {
+        var button = removeCartItemButtons[i]
+        button.addEventListener('click'. removeCartItem)
+    }
+
+    var quantityInputs = document.getElementsByClassName('input-cart-quantity')
+    for (var i = 0; i < quantityInputs.length; i++) {
+        var input = quantityInputs[i]
+        input.addEventListener('change', quantityChange)
+    }
+
+    var addToCartButtons = document.getElementsByClassName('add-to-cart')
+    for (var i = 0; i < addToCartButtons.length; i++) {
+        var button = addToCartButtons[i]
+        button.addEventListener('click', addToCartClicked)
+    }
+
+    document.getElementsByClassName('finish-buying')[0].addEventListener('click', purchaseClicked)
+}
