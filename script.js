ebc2771a7edd612f86498742f123db0ecfcfeb44
n1er4 <102894287+n1er4@users.noreply.github.com>
2022-05-22 17:50:26 +0400

    feat: add container

--- script.js
+++ script.js
@@ -1,9 +1,39 @@
+const container = document.createElement('div');
+container.classList.add('container');
+document.getElementsByTagName('body')[0].prepend(container);
+
 fetch('../assets/JSON.json')
         .then(response => {
             return response.json();
         })
         .then(data => {
             console.log(data);
+        data.forEach((element) => {
+      const bookWrapper = document.createElement('div');
+      bookWrapper.classList.add('container');
+      container.appendChild(bookWrapper);
+
+      const img = document.createElement('img');
+      img.src = element.imageLink;
+      bookWrapper.appendChild(img);
+
+      const author = document.createElement('p');
+      author.innerText = element.author;
+      bookWrapper.appendChild(author);
+
+      const title = document.createElement('h2');
+      title.innerText = element.title;
+      bookWrapper.appendChild(title);
+
+      const price = document.createElement('p');
+      price.innerText = `Price: $${element.price}`;
+      bookWrapper.appendChild(price);
+
+      const description = document.createElement('p');
+      description.innerText = element.description;
+      bookWrapper.appendChild(description);
+    });
+  });
         });
 
 if (document.readyState == 'loading') {
